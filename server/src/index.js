d5977a5f09e4a7dd66f9b78f725c282b0468bf0d
amankhobragade59 <amankhobragade59@gmail.com>
2026-02-02 17:00:47 +0530

    feat: Initialize client Axios instance and server Express application with CORS and weather route.

--- server/src/index.js
+++ server/src/index.js
@@ -8,7 +8,7 @@ const app = express();
 
 //middlewares
 app.use(cors({
-    origin: process.env.CORS_ORIGIN || "http://localhost:5173",
+    origin: process.env.CORS_ORIGIN,
     credentials:true,
 }));
 const port = process.env.PORT;
